6b007c485fdf2ceb36184577d133c44fe73e6430
ao475 <ao475@uis.cam.ac.uk>
2019-11-01 15:54:13 +0000

    update courseServiceBean

--- src/main/java/com/aksam/jeeapp/jpa/servicebean/CourseServiceBean.java
+++ src/main/java/com/aksam/jeeapp/jpa/servicebean/CourseServiceBean.java
@@ -1,35 +1,62 @@
 package com.aksam.jeeapp.jpa.servicebean;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 import com.aksam.jeeapp.jpa.bean.Course;
+import com.aksam.jeeapp.jpa.service.CourseService;
 
 @ManagedBean(name="courseServiceBean")
 @RequestScoped
 public class CourseServiceBean {
 
 	
+private CourseService courseService ;
+	
+	@ManagedProperty(value="#{emFactoryBean}")
+	private EntityManagerFactoryBean factoryBean;
+	
 	@ManagedProperty(value="#{course}")
 	private Course course;
+
 	private String errMsg= null;
-	public Course getCourse() {
-	return course;
-	}
-	public void setCourse(Course course) {
-	this.course = course;
-	}
-	public String getErrMsg() {
-	return errMsg;
-	}
-	public void setErrMsg(String errMsg) {
-	this.errMsg = errMsg;
-	}
-	public String addCourse() {
-	return "listCourse";
-	}
+
+	@PostConstruct
+	public void init() {
+		courseService = new CourseService(factoryBean);
 	}
 	
+	public void setFactoryBean(EntityManagerFactoryBean factoryBean) {
+		this.factoryBean = factoryBean;
+	}	
+	
+	public Course getCourse() {
+		return course;
+	}
 
+	public void setCourse(Course course) {
+		this.course = course;
+	}
 
+	public String getErrMsg() {
+		return errMsg;
+	}
+
+	public void setErrMsg(String errMsg) {
+		this.errMsg = errMsg;
+	}
+	
+	public String addCourse() {
+		courseService.addCourse(course);
+		return "listCourse";
+	}
+	
+	public List<Course> getCourses() {
+		return courseService.getCourses();
+	}
+
+}
